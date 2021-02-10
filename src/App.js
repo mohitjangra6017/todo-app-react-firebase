8dc6dd2f7782b56a8fc0ace2e183610917c3bcbf
Mohit Jangra <mohitjangra6017@gmail.com>
2021-02-11 01:07:40 +0530

    new todo-app update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello Mr Jangra :</h1>
+    <input/>
+    <button>Click me !</button> 
+    </div>
+  );
+}
+
+export default App;
