925d2d485585da9b66030ed0d4bd3ec7db25c8c3
Mohit Jangra <mohitjangra6017@gmail.com>
2021-02-11 01:26:35 +0530

    add file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const[todos, setTodos] = useState([]);
   return (
     <div className="App">
     <h1>Hello Mr Jangra</h1>
     <input/>
     <button>Click me !</button> 
+    <ul>
+      <li>Hey Baby</li>
+      <li>Hey jaan</li>
+
+    </ul>
     </div>
+  
   );
 }
 
