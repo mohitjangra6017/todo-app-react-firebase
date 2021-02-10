49700c7ffb2c25b4809466fe5777b85a76d78c7e
Mohit Jangra <mohitjangra6017@gmail.com>
2021-02-11 01:10:51 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Hello Mr Jangra :</h1>
+    <h1>Hello Mr Jangra</h1>
     <input/>
     <button>Click me !</button> 
     </div>
